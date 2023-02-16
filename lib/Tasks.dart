da1d1c6e1293d15a10f3e216cbd0389689850249
Abhinaba Dash <abhinabadash19@gmail.com>
2023-02-17 01:56:29 +0530

    ui changes

--- lib/Tasks.dart
+++ lib/Tasks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/model/TaskComponents.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'constants/color_constants.dart';
 import 'package:intl/intl.dart';
 
@@ -21,7 +22,6 @@ class ToDoTask extends StatefulWidget {
 }
 
 class _ToDoTaskState extends State<ToDoTask> {
-
   late List<bool> _isChecked;
 
   @override
@@ -37,20 +37,43 @@ class _ToDoTaskState extends State<ToDoTask> {
         itemCount: widget.events.length,
         itemBuilder: (context, index) {
           return Container(
-              padding: EdgeInsets.only(left: 10, right: 15, bottom: 10),
+              padding: EdgeInsets.only(left: 24, right: 24, bottom: 10),
               decoration: BoxDecoration(
                   color: Gcolors.neutralColor900,
                   borderRadius: BorderRadius.circular(20)),
-              child: 
-              ListTile(
-                leading: Container(
-                  child: Text('Checkbox'),
-                ),
-                title: Text(widget.events[index].title),
-                subtitle: Text(DateFormat.yMMMEd().format(widget.events[index].date)),
-                trailing:
-                    Icon(Icons.edit_document, color: Gcolors.primaryColor400),
-                    
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Text('C'),
+                    title: Text(
+                      widget.events[index].title,
+                      style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                        color: Gcolors.primaryColor050,
+                        fontSize: 14,
+                      )),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        Text(
+                          (DateFormat.yMMMEd()
+                                  .format(widget.events[index].date)) +
+                              '-' +
+                              'Time',
+                          style: GoogleFonts.inter(
+                              color: Gcolors.primaryColor400,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    trailing: Icon(Icons.edit_square, color: Gcolors.primaryColor100),
+                  ),
+                  Divider(
+                    thickness: 0.6,
+                    color: Gcolors.neutralColor400,
+                  ),
+                ],
               ));
           // return ToDoTask(
           //     taskName: TaskList[index][0],
